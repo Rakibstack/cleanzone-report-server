bee105f336fd01f462e686922b81a9ca06456fc9
Rakibstack <rh6039511@gmail.com>
2025-11-11 19:20:37 +0600

    added token vafify interceptors function

--- index.js
+++ index.js
@@ -4,13 +4,45 @@ const cors = require('cors')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.port || 3000;
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./clean-zone-client-firebase-adminsdk-.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 
 // middleware.
 app.use(cors());
 app.use(express.json());
 
-// o6s7Mg36267iNuRQ
-// CleanZone-Report
+const verifyfirebasetoken = async (req,res,next) => {
+
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send('unauthorize access')
+    }
+    const token =authorization.split(' ')[1]
+    if(!token){
+        return res.status(401).send('unauthorize access') 
+    }
+
+    try{
+     const decoded = await admin.auth().verifyIdToken(token)
+     req.token_email = decoded.email;
+     console.log(decoded);
+     next()
+     
+    }
+    catch{
+         return res.status(401).send('unauthorize access') 
+    }
+
+
+}
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster01.g0bc8bl.mongodb.net/?appName=Cluster01`;
 
 const client = new MongoClient(uri, {
@@ -27,6 +59,14 @@ async function run() {
         await client.connect()
         const CleanZoneDB = client.db('CleanZoneDB')
         const Allissues = CleanZoneDB.collection('Allissues')
+
+        app.post('/allissues',verifyfirebasetoken, async (req,res) => {
+            
+            const newissue = req.body
+            const result = await Allissues.insertOne(newissue);
+            res.send(result)      
+
+        })
 
      
         await client.db("admin").command({ ping: 1 });
